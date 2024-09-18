275db9b63eb83929cdffef0008178d1a4ff1fe97
devansh rathod <rathoddevansh53@gmail.com>
2024-09-18 23:14:59 +0530

    7th commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -8,29 +8,31 @@ import './App.css'
 //import SP2 from './component/Practice/SP2'
 //import SP3 from './component/Practice/SP3'
 // import A1 from './component/context/A1'
-
 // let Vj = createContext();
+import { useReducer } from 'react';
 
-const App = () => {
-  const first = useRef(null)
-  const submit = (e) => {
-    e.preventDefault()
-    console.log(first.current.value);
+const reducer = (state,action) =>{
+   switch (action.type) {
+    case 'increment':
+      return { count : state.count + 1};
+    case 'decrement':
+      return { count : state.count - 1};
+     default:
+      throw new Error();
+   }
     
-    let name = first.current.value;
-    (name==="devansh")
-    ?
-    alert(" you win bike")
-    :
-    alert("again you win items")
-  }
+}
+const intialstate = { count : 0}
+const App = () => {
+
+  const [state, dispatch] = useReducer(reducer,intialstate)
+
   return (
     <div className="centered-div">
       <div className="content">
-        <form onSubmit={submit} >
-          <input ref={first} />
-          <button>submit</button>
-        </form>
+        Count : {state.count}
+       <button onClick={() =>{dispatch({type: 'decrement'})}}>-</button>
+       <button onClick={() =>{dispatch({type: 'increment'})}}>+</button>
       </div>
     </div>
   );
@@ -55,3 +57,20 @@ export default App
   //  <Vj.Provider value="context api lernnig with sager bhai" >
   //<A1 />
   //</Vj.Provider>
+
+  //const first = useRef(null)
+  // const submit = (e) => {
+  //   e.preventDefault()
+  //   console.log(first.current.value);
+    
+  //   let name = first.current.value;
+  //   (name==="devansh")
+  //   ?
+  //   alert(" you win bike")
+  //   :
+  //   alert("again you win items")
+  // }
+//   <form onSubmit={submit} >
+//   <input ref={first} />
+//   <button>submit</button>
+// </form>
