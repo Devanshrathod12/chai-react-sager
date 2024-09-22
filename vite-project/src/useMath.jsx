a378c44eef6991b02518c3b0f3867a840cd08a40
devansh rathod <rathoddevansh53@gmail.com>
2024-09-23 01:37:19 +0530

    debuggerhouck

--- vite-project/src/useMath.jsx
+++ vite-project/src/useMath.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React ,{useDebugValue, useState}from 'react'
 
 const useMath = (a) => {
+  const [first, setfirst] = useState("")
+  useDebugValue(first)
     let sum = a + 3;
     let nul = a * 3;
+    useDebugValue(sum)
   return [sum,nul]
 }
 
