cb9dd9e0cf44cf1d208a6dae1f740d998cdeac10
devansh rathod <rathoddevansh53@gmail.com>
2024-09-24 23:45:59 +0530

    wether app

--- WetherApp/src/App.jsx
+++ WetherApp/src/App.jsx
@@ -1,31 +1,42 @@
-import { useState,useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import './App.css'
+import img from './assets/img.png'
 
-function App() {
-  const [data, setdata] = useState(null)
-  const [city, setcity] = useState("indore")
- function fun(e){
-  setcity(e.target.value)
-  console.log(e.target.value);
- }
+const App = () => {
+  const [data, setData] = useState(null);
+  const [city, setCity] = useState("indore");
+
+  function func(e) {
+    setCity(e.target.value);
+    console.log(e.target.value);
+  }
+
   useEffect(() => {
-    const fechapi = async ()=>{
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=455880e4c2b6874a74014041200ab80d`;
-      const responce = await fetch(url)
-      const result = await responce.json()
-      setdata(result.main);
-      console.log(result);
-    }
-    fechapi()
-  }, [city])
-  
+    const fetchApi = async () => {
+      try {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=455880e4c2b6874a74014041200ab80d`;
+        const response = await fetch(url);
+        const result = await response.json();
+        console.log(result);
+        setData(result.main);
+      } catch (error) {
+        console.error("Error fetching weather data:", error);
+      }
+    };
+    fetchApi();
+  }, [city]);
 
   return (
-    <>
-     <input type="text" className='bg-black text-white' onChange={fun}/>
-       {data.temp}
+    <> 
+    <div id="h" className="bg-green-900 w-[80rem] h-[100vh] m-[-2rem] " >
+      <input type="text" className="bg-black text-white mt-10 pt-3 font-extrabold text-center rounded-[2rem]" onChange={func} />
+      <div className="font-extrabold text-black-900 text-[2rem] mt-4">
+        <div className="w-[20rem] h-[20rem] bg-black ml-[30rem] mt-4 text-white rounded-[4rem] font-extrabold text-2xl" >
+          <img src={img} alt="" className="w-[200px] h-[200px] pt-9 pl-14 rounded-3xl" />
+          {data ? data.temp : "Loading..."}</div></div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
